0d6d4cd66cea7879aaa236fd98ae9dca554d2b7c
Nora Sandler <nlsandler@gmail.com>
2017-10-18 21:49:17 -0400

    handle escape sequences for character constants

--- src/lex.ml
+++ src/lex.ml
@@ -13,7 +13,6 @@ module Lex : sig
         | Int of int
         | Char of char
         | Id of string
-    (* type token *)
     val lex : string -> token list
     val tok_to_string: token -> string
 end =
@@ -29,22 +28,67 @@ end =
             | ReturnKeyword
             | Int of int
             | Char of char
-            | Id of string 
+            | Id of string
+        (*
+            int_regexp: (-?[0-9]+)(\b.* ) 
+        *) 
         let int_regexp = Str.regexp "\\(-?[0-9]+\\)\\(\\b.*\\)" (* TODO: handle more int representations (e.g. hex) *)
-        let id_regexp = Str.regexp "\\([A-Za-z][A-Za-z0-9_]*\\)\\(\\b.*\\)" 
-        let char_regexp = Str.regexp "'[^\\\\]'\\(.*\\)" (* TODO: handle escape sequences *)
+        (*
+            id_regexp: ([A-Za-z][A-Za-z0-9_]* )(\b.* ) 
+        *)
+        let id_regexp = Str.regexp "\\([A-Za-z][A-Za-z0-9_]*\\)\\(\\b.*\\)"
+        (*
+            char_regexp: ('[^\\]|\\([abfenrtv'?]|[0-7]{1,3}|[0-9a-f]{1,3})')(.* )
+            in other words:
+            -a single quote
+            -one character that isn't a backslash or single quote, OR
+            -a backslash followed by:
+                -a, b, f, e, n, r, t, v, backslash, question mark, single quote or double quote (escape sequences), OR
+                -1, 2, or 3 digits between 0 and 7 (octal representation)
+                -h, followed by 1, 2, or 3 digits between 0-9, a-f (hex representation)
+            -a single closing quote
+            -everything else
+        *)
+        let char_regexp = Str.regexp "'\\([^'\\\\]\\|\\\\\\([abfenrtv'\"?]\\|[0-7]+\\|x[0-9a-f]+\\)\\)'\\(.*\\)"
+
+        let get_char char_token = 
+            if String.length char_token = 1 then String.get char_token 0 else
+            if String.length char_token = 2 then (* escape sequence *)
+                match char_token with
+                | "\\" -> Char.chr 92   (* backslash *)
+                | "\\a" -> Char.chr 7   (* bell *)
+                | "\\b" -> '\b'         (* backspace *)
+                | "\\e" -> Char.chr 27  (* esc *)
+                | "\\f" -> Char.chr 12  (* form feed *)
+                | "\\n" -> '\n'         (* newline *)
+                | "\\r" -> '\r'         (* carriage return *)
+                | "\\t" -> '\t'         (* tab *)
+                | "\\v" -> Char.chr 11  (* vertical tab *)
+                | "\\\'" -> Char.chr 39 (* single quote *)
+                | "\\\"" -> '"'         (* double quote *)
+                | "\\?" -> '?'          (* question mark *)
+                | _ -> failwith("Unknown escape sequence "^char_token) else
+            if String.get char_token 1 = 'x' then (* hex string *)
+                let num_str = "0"^(String.slice ~first:1 char_token) in
+                Char.chr (Int.of_string num_str) 
+            else 
+                let num_str = "0o"^(String.slice ~first:1 char_token) in
+                Char.chr (Int.of_string num_str)
+
         let get_const_or_id input =
             if Str.string_match int_regexp input 0
-            then 
+            then (* it's an int *)
                 let int_token = Str.matched_group 1 input in
                 let rest = Str.matched_group 2 input in
                     (Int(int_of_string int_token), rest)
             else if Str.string_match char_regexp input 0
-            then
-                let rest = Str.matched_group 1 input in
-                    (Char(String.get input 1), rest)
+            then (* it's a char *)
+                let char_token = Str.matched_group 1 input in
+                let rest = Str.matched_group 3 input in
+                let c = get_char char_token in 
+                (Char(c), rest)
             else if Str.string_match id_regexp input 0
-            then
+            then (* it's an ID, possibly a keyword *)
                 let id_token_str = Str.matched_group 1 input in
                 let rest = Str.matched_group 2 input in
                 let id_token = 
