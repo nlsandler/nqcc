6e9a1b5c08d73773dab5dc883aa824822966177e
Nora Sandler <nlsandler@gmail.com>
2017-10-26 14:23:49 -0400

    refactoring test script a bit

---
+++ test_examples.sh
@@ -0,0 +1,21 @@
+for i in examples/*.c
+do
+    gcc -m32 $i             #compile with gcc
+    ./a.out                 #run it
+    expected=$?             #get exit code
+    ./nqcc $i              #compile with nqcc
+    base="${i%.*}"
+    $base                  #run the thing we assembled
+    actual=$?                #get exit code
+    echo -n "$i:    "
+    if [ "$expected" -ne "$actual" ]
+    then
+        echo "FAIL"
+    else
+        echo "OK"
+    fi
+    rm $base
+done
+
+#cleanup
+rm a.out
